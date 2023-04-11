cb5a6c54cc8d9c244c1a8e108afb2e86363eb711
Luke Zhang <you@example.com>
2023-04-10 18:48:03 -0600

    completed boilerplate

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -74,14 +74,12 @@ const Browser = require('zombie');
 Browser.site = 'http://localhost:3000/';
 
 suite('Functional Tests with Zombie.js', function () {
+  this.timeout(5000);
   const browser = new Browser();
 
   suiteSetup(function(done) {
-    return browser.visit('/', done);
+    return browser.visit('/', done());
   });
-
-  this.timeout(5000);
-
 
   suite('Headless browser', function () {
     test('should have a working "site" property', function() {
@@ -92,15 +90,27 @@ suite('Functional Tests with Zombie.js', function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', 'Colombo').then(()=> {
+        browser.pressButton('submit', ()=> {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Cristoforo');
+          browser.assert.text('span#surname', 'Colombo');
+          browser.assert.elements('span#dates', 1);
+          done();
+        })
+      });
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', 'Vespucci').then(()=>{
+        browser.pressButton('submit', ()=> {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Amerigo');
+          browser.assert.text('span#surname', 'Vespucci');
+          browser.assert.elements('span#dates', 1);
+          done();
+        })
+      })
     });
   });
 });
